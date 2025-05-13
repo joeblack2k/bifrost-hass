703ae67f6d09ebf2fe6f4b13eafad46f63ead105
Christian Iversen <ci@iversenit.dk>
2025-05-13 10:02:30 +0200

    + Add base_url() helper and new context signal functions

--- crates/bifrost-frontend/src/lib.rs
+++ crates/bifrost-frontend/src/lib.rs
@@ -4,3 +4,20 @@
 pub mod icons;
 pub mod toast;
 pub mod traits;
+
+use dioxus::prelude::*;
+
+#[must_use]
+pub fn base_url() -> String {
+    web_sys::window().unwrap().location().origin().unwrap()
+}
+
+pub fn use_context_signal_provider<T: 'static>(f: impl FnOnce() -> T) -> Signal<T, UnsyncStorage> {
+    let inner = use_signal(f);
+    use_context_provider(move || inner)
+}
+
+#[must_use]
+pub fn use_context_signal<T>() -> Signal<T> {
+    use_context::<Signal<T>>()
+}
